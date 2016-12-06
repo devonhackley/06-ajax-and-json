d6bf9aeed0ba0d39d2b012d1f00205a1c34af04f
devonhackley <devonhackley@gmail.com>
2016-12-06 07:54:54 -0800

    I think I have everything working correctly

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -46,12 +46,19 @@ Article.fetchAll = function() {
     /* When our data is already in localStorage:
     1. We can process and load it,
     2. Then we can render the index page.  */
+    Article.loadAll(JSON.parse(localStorage.blogArticles));
+    articleView.renderIndexPage();
   } else {
     /* Without our localStorage in memory, we need to:
     1. Retrieve our JSON file with $.getJSON
       1.a Load our json data
       1.b Store that data in localStorage so that we can skip the server call next time,
       1.c And then render the index page.*/
+    $.getJSON('data/blogArticles.json', function(data) {
+      Article.loadAll(data);
+      localStorage.setItem('blogArticles',JSON.stringify(data));
+      articleView.renderIndexPage();
+    });
   }
 };
 
